cce291c9c35c76701ccc5d6697371eecd0ecb475
gao171024 <15511692608@163.com>
2017-09-06 17:32:01 +0800

    service_one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {MaterialModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
 import { LoggerService } from './service/logger.service';
 import { ConsoleService } from './service/console.service';
 import 'rxjs/';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [
     ConsoleService,
